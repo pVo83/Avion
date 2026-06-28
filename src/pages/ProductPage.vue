09369b83243b59935989d8523d1f1e7e413af4bb
pVo83 <99754427+pVo83@users.noreply.github.com>
2026-06-28 21:23:55 +0300

    fixed: importing the list to allListings

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -12,7 +12,7 @@
 <script setup>
 import { computed } from "vue"
 import { useRoute } from "vue-router"
-import { listings } from "@/components/model/listings"
+import { allListings } from "@/components/model/listings"
 import ProductDetails from "@/components/section/ProductDetails.vue"
 import Listings from "@/components/section/Listings.vue"
 import Features from "@/components/section/Features.vue"
@@ -21,7 +21,7 @@ import Newsletter from "@/components/section/Newsletter.vue"
 const route = useRoute()
 
 const product = computed(() => {
-  return listings.find((listing) => listing.id === Number(route.params.id))
+  return allListings.find((listing) => listing.id === Number(route.params.id))
 })
 </script>
 
